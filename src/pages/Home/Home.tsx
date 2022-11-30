0747fea757ab5c70653bfd06758a2f8eb149d959
Dmytro <ryliov.work@gmail.com>
2022-11-30 20:19:11 +0100

    refactor: setting data from firestore in rtk

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,23 +1,39 @@
 //react
-import React from 'react';
+import React, { useEffect } from 'react';
 //rtk
-import { iUserSlice } from "../../app/user-slice";
+import { useAppDispatch, useAppSelector } from "../../hooks/hooks";
+import { iUserSlice, setAllData } from "../../app/user-slice";
+//firebase
+import { useDocumentData } from "react-firebase-hooks/firestore";
+import { doc } from "firebase/firestore";
+import FirebaseConfig from "../../firebase/firebase.config";
 //components
 import Components from "../../components";
 //styles
 import './Home.scss'
 
 const Home: React.FC = () => {
-  // if (dataLoading) {
-  //   return <Components.Loader />
-  // }
+  const dispatch = useAppDispatch()
+  const {uuid, columns} = useAppSelector(state => state.userSlice)
+  const [userData, userDataLoading] = useDocumentData(doc(FirebaseConfig.firestoreDB, 'users', uuid))
+
+  //posting data from firestore to local global storage
+  useEffect(() => {
+    if (userData) {
+      dispatch(setAllData(userData as iUserSlice))
+    }
+  }, [userData])
+
+  if (userDataLoading) {
+    return <Components.Loader />
+  }
 
   return (
     <div className="home">
       <Components.MainInfo />
       <div className="home__charts">
-        {/*<Components.DoughnutChart data={data!} />*/}
-        {/*<Components.LineChart data={data! as iUserSlice} />*/}
+        <Components.DoughnutChart columns={columns} />
+        <Components.LineChart columns={columns} />
       </div>
     </div>
   );
