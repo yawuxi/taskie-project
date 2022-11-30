81b24377acb455d9e24b6e19d92473a721bb74cb
Dmytro <ryliov.work@gmail.com>
2022-11-30 20:56:33 +0100

    refactor: getting uuid in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
 //react
 import React from 'react';
+//rtk
+import { useAppDispatch } from "./hooks/hooks";
+import { setUUID } from "./app/user-slice";
+//firebase
+import { useAuthState } from "react-firebase-hooks/auth";
+import FirebaseConfig from "./firebase/firebase.config";
 //components
 import { AppRouter } from "./AppRouter";
+import Components from "./components";
 //styles
 import './App.scss'
 
 const App: React.FC = () => {
+  const dispatch = useAppDispatch()
+  const [user, userLoading] = useAuthState(FirebaseConfig.firebaseAUTH)
+
+  if (userLoading) {
+    return <Components.Loader />
+  }
+
+  //put uuid to the redux storage
+  if (user) {
+    dispatch(setUUID(user.uid))
+  }
+
   return (
     <div className="app" data-color-theme="dark">
-      <AppRouter />
+      <AppRouter user={user}/>
     </div>
   );
 };
